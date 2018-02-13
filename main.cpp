2588369e6450d4d2a72c79bdaf88f313971c4ed5
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-13 11:12:11 -0800

    Progam will display nickname.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main()
   cout << "What is your last name?\n";
   cin >> Last;
   //tell fortune
-
+  cout << "Welcome," << First[0] << Last[0];
+  cout << ",here is your fortune...\n";
   return 0;
 }
