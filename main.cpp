7a276564b0c48dbaa663581cc03ec610d2f1d8cc
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-13 11:18:12 -0800

    Progam with display lucky number which is equal to the users first and last name.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main()
 {
   //declare variables
   string First;
-  string Last; 
+  string Last;
+  int lucky; 
   //get user input
   cout << "What is your first name?\n"; 
   cin >> First; 
@@ -17,5 +18,6 @@ int main()
   //tell fortune
   cout << "Welcome," << First[0] << Last[0];
   cout << ",here is your fortune...\n";
+  cout << "your lucky number is " << First.length() + Last.length() <<endl;
   return 0;
 }
