4de89ef87fc6a8e1c5570f6b900e76ca0f613aa7
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-13 10:55:42 -0800

    The progam asks for first and last name.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Joseluis Zamora
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string First;
+  string Last; 
   //get user input
-
+  cout << "What is your first name?\n"; 
+  cin >> First; 
+  cout << "What is your last name?\n";
+  cin >> Last;
   //tell fortune
 
   return 0;
